54db9d61a85fa7b3325ac983b467e889030a4aa2
Shubham singh <rajputshubhamsingh593@gmail.com>
2022-07-26 16:03:44 +0530

    try to use git bash

---
+++ Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+    // Holle World greeting;
+    public static void main(String[] args) {
+
+        greeting();
+    }
+
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
---
+++ lolo.java
@@ -0,0 +1,5 @@
+public class lolo {
+    public static void main(String[] args) {
+        System.out.println("lolo world!");
+    }
+}
